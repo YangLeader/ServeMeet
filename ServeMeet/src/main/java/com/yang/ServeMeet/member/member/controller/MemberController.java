4a1fe061cdc35a0396ce2f2f2adfe6e324a9238e
wldnjs1040 <43400152+wldnjs1040@users.noreply.github.com>
2018-12-20 09:14:25 +0900

    멤버 수정
    마이바티스 등등 뭔가 추가..

---
+++ ServeMeet/src/main/java/com/yang/ServeMeet/member/member/controller/MemberController.java
@@ -0,0 +1,16 @@
+package com.yang.serveMeet.member.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@SessionAttributes(value= {"member"})
+@Controller
+public class MemberController {
+	
+	private Logger logger = LoggerFactory.getLogger(MemberController.class);
+	
+	
+
+}
---
+++ ServeMeet/src/main/java/com/yang/ServeMeet/member/member/model/vo/Member.java
@@ -0,0 +1,121 @@
+package com.yang.serveMeet.member.model.vo;
+
+public class Member {
+	
+	
+	private static final long serialVersionUID = 1000L;
+	
+	private String userNo;
+	private String userId;
+	private String userPwd;
+	private String userName;
+	private String email;
+	private String point;
+	
+	
+	public Member() {
+		super();
+	}
+	
+
+
+	public Member(String userId, String userPwd) {
+		super();
+		this.userId = userId;
+		this.userPwd = userPwd;
+	}
+
+
+	public Member(String userId, String userPwd, String userName, String email) {
+		super();
+		this.userId = userId;
+		this.userPwd = userPwd;
+		this.userName = userName;
+		this.email = email;
+	}
+
+
+
+
+	public Member(String userNo, String userId, String userPwd, String userName, String email) {
+		super();
+		this.userNo = userNo;
+		this.userId = userId;
+		this.userPwd = userPwd;
+		this.userName = userName;
+		this.email = email;
+	}
+
+
+
+	public String getUserNo() {
+		return userNo;
+	}
+
+
+	public void setUserNo(String userNo) {
+		this.userNo = userNo;
+	}
+
+
+	public String getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+
+	public String getUserPwd() {
+		return userPwd;
+	}
+
+
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPoint() {
+		return point;
+	}
+
+
+	public void setPoint(String point) {
+		this.point = point;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Member [userNo=" + userNo + ", userId=" + userId + ", userPwd=" + userPwd + ", userName=" + userName
+				+ ", email=" + email + ", point=" + point + "]";
+	}
+	
+	
+	
+	
+
+}
