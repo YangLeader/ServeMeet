48bf9a3f2964ea8290d63090d4b48d04795920a5
SeungIn <ingsn722@gmail.com>
2018-12-20 15:19:33 +0900

    batting test commit

---
+++ ServeMeet/src/main/java/com/yang/ServeMeet/batting/model/dao/BattingDao.java
@@ -0,0 +1,9 @@
+package com.yang.ServeMeet.batting.model.dao;
+
+import com.yang.ServeMeet.batting.model.vo.Batting;
+
+public interface BattingDao {
+	
+	Batting battingSelect(int battingId);
+
+}
---
+++ ServeMeet/src/main/java/com/yang/ServeMeet/batting/model/service/BattingService.java
@@ -0,0 +1,9 @@
+package com.yang.ServeMeet.batting.model.service;
+
+import com.yang.ServeMeet.batting.model.vo.Batting;
+
+public interface BattingService {
+
+	
+	Batting battingSelect(int battingId);
+}
