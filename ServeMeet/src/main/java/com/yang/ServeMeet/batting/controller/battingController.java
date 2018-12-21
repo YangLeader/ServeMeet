c921c3eb15caff4a92b10dc39b7e8835ea0df072
SeungIn <ingsn722@gmail.com>
2018-12-21 15:18:45 +0900

    배팅 선택

--- ServeMeet/src/main/java/com/yang/ServeMeet/batting/controller/battingController.java
+++ ServeMeet/src/main/java/com/yang/ServeMeet/batting/controller/battingController.java
@@ -1,17 +1,58 @@
 package com.yang.ServeMeet.batting.controller;
 
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.yang.ServeMeet.batting.model.service.BattingService;
+import com.yang.ServeMeet.batting.model.vo.BattingUser;
 
 @Controller
 public class battingController {
 
+	
+	@Autowired
+	private BattingService battingService;	
+		
+	
 	@RequestMapping("/batting/battingInfo.do")
-	public void battingInfo(Model model) {
+	public String battingInfo(@RequestParam int no, Model model) {
+		
+		model.addAttribute("batting",battingService.battingSelect(no));
+		
+		return "batting/battingInfo";
+	}
+	
+	@RequestMapping("/batting/battingList.do")
+	public String battingList(Model model) {
+		
+		ArrayList<Map<String,String>> list = new ArrayList<Map<String,String>>(battingService.battingList());
+		
+		model.addAttribute("list",list);
 		
 		
+		return "batting/battingList";
+	}
+	
+	@RequestMapping("/batting/battingPick.do")
+	public String battingPick(@RequestParam int battingId ,@RequestParam String battingSelect,@RequestParam String userName, Model model) {
 		
-		model.addAttribute("");
+		
+		int result = battingService.battingPick(battingId, battingSelect);
+		
+		BattingUser bUser = new BattingUser(battingId,userName,battingSelect);
+		
+		int result2 = battingService.battingPickUser(bUser);
+		
+		ArrayList<Map<String,String>> list = new ArrayList<Map<String,String>>(battingService.battingList());
+		
+		model.addAttribute("list",list);
+		
+		return "batting/battingList";
 	}
 }
--- ServeMeet/src/main/java/com/yang/ServeMeet/batting/model/dao/BattingDao.java
+++ ServeMeet/src/main/java/com/yang/ServeMeet/batting/model/dao/BattingDao.java
@@ -1,9 +1,18 @@
 package com.yang.ServeMeet.batting.model.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import com.yang.ServeMeet.batting.model.vo.Batting;
+import com.yang.ServeMeet.batting.model.vo.BattingUser;
 
 public interface BattingDao {
 	
 	Batting battingSelect(int battingId);
 
+	List<Map<String,String>> battingList();
+	
+	int battingPick(Map<String, Object> hmap);
+	
+	int battingPickUser(BattingUser bUser);
 }
